bbb2936930a935dc35f84bc9f432fbd280c40cb7
Tech-It-Biz <tech.we@outlook.com>
2025-10-04 19:00:19 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,21 +12,6 @@ export default function RootLayout({ children }) {
       <body>
         <AuthSessionProvider>
           {children}
-
-          {/* Tawk Script */}
-          <Script id="tawk-to" strategy="afterInteractive">
-            {`  
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/68dcc3413f11aa194e287b0d/1j6f8sna0';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-            `}
-          </Script>
         </AuthSessionProvider>
       </body>
     </html>
